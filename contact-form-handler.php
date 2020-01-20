4aae9b96b9c4f3abcf3b0450260eedd86d1edbb0
kayronmacon <45487610+kayronmacon@users.noreply.github.com>
2020-01-20 09:36:28 -0500

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -13,10 +13,12 @@
 
     $to = "kayronmacon@gmail.com";
 
-    $headers = "From $email_from \r\n"
+    $headers = "From $email_from \r\n";
 
     $headers .= "Reply-To: $visitor_email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
 
     header("Location: index.html")
+
+    ?>
